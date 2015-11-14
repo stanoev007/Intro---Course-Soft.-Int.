ca9e0a5e6e1406a5d4ef54b754356d925cce6879
stanoev007 <boyan.stanoev@gmail.com>
2015-11-14 19:51:34 +0200

    homework 8 , 1st exercise

---
+++ Homework 8/1st/src/sm1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class sm1 {
+
+	public static void main(String[] args) {
+		Scanner a = new Scanner(System.in);
+		System.out.println("Въведете име: ");
+		String b = a.nextLine();
+		n(b);
+		a.close();
+
+	}
+	public static void n(String name){
+		System.out.println("Greetings, " + name);
+	}
+
+}
